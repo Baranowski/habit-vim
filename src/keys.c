d04d9ee98cba0f851b4bbb0abecbee72aef39107
Wojciech Baranowski <w.baranowski@o2.pl>
2013-06-08 22:00:45 +0200

    keys: Print ctrl combos
    Too greedy, still needs some refining

--- src/keys.c
+++ src/keys.c
@@ -12,9 +12,19 @@ struct key_mapping {
 static const struct key_mapping keys_map[] =
     { { ' ',    "<Space>"                  }
     , { '\t',   "<Tab>"                    }
+    , { '\x1b', "<Esc>"                    }
+    , { '\n',   "<Ret>"                    }
+    , { '\x7f', "<Backspace>"              }
     , { '\0',   NULL                       }
     };
 
+#define MAX_CTRL_COMBO 26
+#define MIN_CTRL_COMBO 1
+
+static const char *ctrl_hreadable_tpl = "<C-x>";
+#define CTRL_COMBO_SUBST_POS 3
+
+//TODO: handle ctrl combos
 char key_from_hreadable(const char *hreadable) {
     if (strlen(hreadable) == 1) {
         return hreadable[0];
@@ -40,6 +50,12 @@ char *hreadable_from_key(char key) {
         result = (char *)calloc(2, sizeof(result[0]));
         result[0] = key;
         //result[1] has been set to 0 by calloc
+    } else if (MIN_CTRL_COMBO <= key && key <= MAX_CTRL_COMBO) {
+        size_t len = strlen(ctrl_hreadable_tpl);
+        result = (char *)calloc(len + 1, sizeof(result[0]));
+        strncpy(result, ctrl_hreadable_tpl, len);
+        result[CTRL_COMBO_SUBST_POS] = 'A' + key - MIN_CTRL_COMBO;
+
     } else {
         const struct key_mapping *it;
         for (it = keys_map;
